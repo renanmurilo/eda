f488a20dbd96b40ea322b9af5ac28827b31c12e4
Renan Murilo dos Santos Gomes <renanmurilosantosg@gmail.com>
2023-02-25 15:18:49 -0300

    criação da página fornos e tornos

--- page-fornos-e-tornos.php
+++ page-fornos-e-tornos.php
@@ -1,10 +1,102 @@
 <?php
 // Template Name: Fornos e Tornos
 get_header();
+?><?php
+if (function_exists('bcn_display')) {
+    bcn_display();
+}
 ?>
     <?php if(have_posts()) { while (have_posts()) { the_post(); ?>
         <main>
+            <section class="section__home__banner">
+                <div class="content__banner slide-banners">
+                    <?php if(have_rows('banners')): while(have_rows('banners')) : the_row(); ?>
+                        <a href="<?php the_sub_field('link'); ?>" class="banner__image">
+                            <img class="banner__desktop" src="<?php the_sub_field('imagem_desktop'); ?>" alt="">
+                            <img class="banner__mobile" src="<?php the_sub_field('imagem_mobile'); ?>" alt="">
+                        </a>
+                    <?php endwhile; else : endif; ?>
+                </div>
+            </section>
 
+            <section class="section__fornos">
+                <div class="shell">
+                    <div class="content__fornos">
+                        <div class="image__desktop">
+                            <img src="<?php the_field('imagem_fornos_desktop'); ?>" alt="<?php the_field('titulo_fornos'); ?>">
+                        </div>
+
+                        <div class="description">
+                            <h1><?php the_field('titulo_fornos'); ?></h1>
+                            <div class="text">
+                                <?php the_field('texto_fornos'); ?>
+                            </div>
+
+                            <div class="icons">
+                                <?php if(have_rows('icones_fornos')): while(have_rows('icones_fornos')) : the_row(); ?>
+                                <div class="icon">
+                                    <div class="image">
+                                        <img src="<?php the_sub_field('icone'); ?>" alt="<?php the_sub_field('titulo'); ?>">
+                                    </div>
+
+                                    <h3><?php the_sub_field('titulo'); ?></h3>
+                                </div>
+                                <?php endwhile; else : endif; ?>
+                            </div>
+
+                            <a href="/fornos" class="btn btn__primary">Conheça os fornos</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section__tornos">
+                <div class="shell">
+                    <div class="content__tornos">
+                        <div class="description">
+                            <h2><?php the_field('titulo_tornos'); ?></h2>
+                            <div class="text">
+                                <?php the_field('texto_tornos'); ?>
+                            </div>
+
+                            <div class="icons">
+                                <?php if(have_rows('icones_tornos')): while(have_rows('icones_tornos')) : the_row(); ?>
+                                <div class="icon">
+                                    <div class="image">
+                                        <img src="<?php the_sub_field('icone'); ?>" alt="<?php the_sub_field('titulo'); ?>">
+                                    </div>
+
+                                    <h3><?php the_sub_field('titulo'); ?></h3>
+                                </div>
+                                <?php endwhile; else : endif; ?>
+                            </div>
+
+                            <a href="/tornos" class="btn btn__primary">Veja os tornos</a>
+                        </div>
+
+                        <div class="image__desktop">
+                            <img src="<?php the_field('imagem_tornos_desktop'); ?>" alt="<?php the_field('titulo_tornos'); ?>">
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section__skutt">
+                <div class="shell">
+                    <div class="content__skutt">
+                        <div class="image__desktop">
+                            <img src="<?php the_field('imagem_skutt_desktop'); ?>" alt="<?php the_field('titulo_skutt'); ?>">
+                        </div>
+                        
+                        <div class="description">
+                            <h2><?php the_field('titulo_skutt'); ?></h2>
+                            <div class="text">
+                                <?php the_field('texto_skutt'); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     <?php } } ?>
 
---
+++ page-fornos.php
@@ -0,0 +1,146 @@
+<?php
+// Template Name: Fornos
+get_header();
+?>
+    <?php if(have_posts()) { while (have_posts()) { the_post(); ?>
+        <main>
+            <section class="section__breadcrumb">
+                <div class="shell">
+                    <div class="content__breadcrumb"></div>
+                </div>
+            </section>
+
+            <section class="section__video">
+                <div class="shell">
+                    <div class="content__video">
+                        <h1><?php the_field('titulo_video'); ?></h1>
+
+                        <div class="video">
+                            <?php the_field('video'); ?>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section__fornos">
+                <div class="shell">
+                    <div class="content__fornos">
+                        <h2>Fornos</h2>
+
+                        <div class="inner__fornos"></div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section__precos">
+                <div class="shell">
+                    <div class="content__precos">
+                        <h2>Lista de preços</h2>
+
+                        <div class="inner__precos">
+                            <div class="header__precos">
+                                <h2>Preços</h2>
+                            </div>
+
+                            <table>
+                                <thead>
+                                    <th>Modelo</th>
+                                    <th>Automático <br>Km</th>
+                                    <th>Touch kmt</th>
+                                    <th>Cone</th>
+                                    <th>Temp ºC</th>
+                                    <th>Diâmetro <br> Interno (Cm)</th>
+                                    <th>Altura Interna (Cm)</th>
+                                    <th>Kit prateleira</th>
+                                    <th>Espessura <br>prateleira</th>
+                                </thead>
+
+                                <tbody>
+                                    <?php if(have_rows('lista_de_precos')): while(have_rows('lista_de_precos')) : the_row(); ?>
+                                    <tr>
+                                        <td><?php the_sub_field('modelo'); ?></td>
+                                        <td><?php the_sub_field('automatico_km'); ?></td>
+                                        <td><?php the_sub_field('touch_kmt'); ?></td>
+                                        <td><?php the_sub_field('cone'); ?></td>
+                                        <td><?php the_sub_field('temp_ºc'); ?></td>
+                                        <td><?php the_sub_field('diametro_interno_cm'); ?></td>
+                                        <td><?php the_sub_field('altura_interna_cm'); ?></td>
+                                        <td><?php the_sub_field('kit_prateleira'); ?></td>
+                                        <td><?php the_sub_field('espessura_prateleira'); ?></td>
+                                    </tr>
+                                    <?php endwhile; else : endif; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section__informacoes">
+                <div class="shell">
+                    <div class="content__informacoes">
+                        <h2>Informações importantes</h2>
+                        
+                        <div class="inner__informacoes">
+                            <?php if(have_rows('informacoes_importantes')): while(have_rows('informacoes_importantes')) : the_row(); ?>
+                            <div class="box">
+                                <div class="image">
+                                    <img src="<?php the_sub_field('icone'); ?>" alt="<?php the_sub_field('titulo'); ?>">
+                                </div>
+
+                                <div class="text">
+                                    <h3><?php the_sub_field('titulo'); ?></h3>
+                                    <div class="text">
+                                        <?php the_sub_field('texto'); ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php endwhile; else : endif; ?>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section__faixa__pagamento">
+                <div class="shell">
+                    <div class="content__faixa__pagamento">
+                        <div class="texto__pagamento">
+                            <h2><?php the_field('titulo_pagamento'); ?></h2>
+                            <div class="text">
+                                <?php the_field('texto_pagamento'); ?>
+                            </div>
+                        </div>
+
+                        <div class="texto__informacoes">
+                            <h2><?php the_field('titulo_outras_informacoes'); ?></h2>
+                            <ul>
+                                <?php if(have_rows('informacoes')): while(have_rows('informacoes')) : the_row(); ?>
+                                <li><?php the_sub_field('texto'); ?></li>
+                                <?php endwhile; else : endif; ?>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section__manual">
+                <div class="shell">
+                    <div class="content__manual">
+                        <div class="image__desktop">
+                            <img src="<?php the_field('imagem_manual_desktop'); ?>" alt="<?php the_field('titulo_manual'); ?>">
+                        </div>
+
+                        <div class="description">
+                            <h2><?php the_field('titulo_manual'); ?></h2>
+                            <div class="text">
+                                <?php the_field('texto_manual'); ?>
+                            </div>
+                            <a href="<?php the_field('arquivo_manual'); ?>" class="btn btn__primary" download>Baixe o manual</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    <?php } } ?>
+
+<?php get_footer(); ?>
